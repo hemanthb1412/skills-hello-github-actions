40bd64b18b9afb021561529b24a5c1dccff6e6a6
Hemanth-tf <hemanth.bhavana@taylorandfrancis.com>
2026-09-16 11:46:44 +0530

    Add HelloWorld class with main method

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Actions!");
+        System.out.println("Java + Maven CI is working!");
+    }
+}
